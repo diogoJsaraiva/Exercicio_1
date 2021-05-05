457f5676db91076079a83e04e10ecebeb86d7d6b
1703703 <diogo.saraiva12@hotmail.com>
2021-05-05 10:13:15 +0100

    Mostrar a informação na seguinte atividade

--- app/src/main/java/com/example/exercicio_1/MostraDadosActivity.kt
+++ app/src/main/java/com/example/exercicio_1/MostraDadosActivity.kt
@@ -2,10 +2,23 @@ package com.example.exercicio_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import pt.ipg.exercicio1.MainActivity
 
 class MostraDadosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mostra_dados2)
+        setContentView(R.layout.activity_mostra_dados)
+
+       val nome = intent.getStringExtra(MainActivity.INFO_EXTRA_NOME)
+       val email = intent.getStringExtra(MainActivity.INFO_EXTRA_EMAIL)
+       val telefone = intent.getStringExtra(MainActivity.INFO_EXTRA_TELEFONE)
+       val idade = intent.getIntExtra(MainActivity.INFO_EXTRA_IDADE,0)
+
+        findViewById<TextView>(R.id.textViewNome).text = nome
+        findViewById<TextView>(R.id.textViewEmail).text = email
+        findViewById<TextView>(R.id.textViewTelefone).text = telefone
+        findViewById<TextView>(R.id.textViewIdade).text = idade.toString()
+
     }
 }
